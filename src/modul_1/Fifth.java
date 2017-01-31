304884f3428668f7a976c4c2d9fc21982b876fbc
Max <axijiji#rambler.ru>
2017-01-31 21:53:14 +0200

    init commit

---
+++ src/modul_1/Fifth.java
@@ -0,0 +1,10 @@
+package modul_1;
+
+/**
+ * Created by Max on 31.01.2017.
+ */
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
---
+++ src/modul_1/SecondClass.java
@@ -0,0 +1,10 @@
+package modul_1;
+
+/**
+ * Created by Max on 31.01.2017.
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
